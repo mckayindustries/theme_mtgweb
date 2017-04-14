0695747d12c5cafc205c089d3e67fcdae293193d
Tre`Von McKay <trevon.mckay@gmail.com>
2017-04-14 16:47:00 -0400

    Initial commit

---
+++ templates/page.html.twig.php
@@ -0,0 +1,40 @@
+<div id="header">
+    <?php if (!is_null($site_name)): ?>
+    <div id="site-title">
+        <h1 class="title is-1"><?php print $site_name; ?></h1>
+    </div>
+    <?php endif; ?>
+    <?php $main_menu = menu_navigation_links('main-menu'); ?>
+    <div id="main-menu">
+        <?php print theme('links__menu_main_menu', array('links' => $main_menu)); ?>
+    </div>
+</div>
+<div id="wrapper">
+    <?php if ($page['featured_links']): ?>
+    <div id="featured-links">
+        <?php print render($page['featured_links']); ?>
+    </div>
+    <?php endif; ?>
+    <?php if ($page['sidebar_first']): ?> 
+    <div id="sidebar-first">
+        <?php print render($page['sidebar_first']); ?>
+    </div>
+    <?php endif; ?>
+    <div id="page-content">
+        <?php print render($page['content']); ?>
+    </div>
+    <?php if ($page['sidebar_second']): ?> 
+    <div id="sidebar-second">
+        <?php print render($page['sidebar_second']); ?>
+    </div>
+    <?php endif; ?>
+</div>
+<footer class="footer" id="footer">
+    <div class="container">
+        <div class="content has-text-centered">
+            <?php if ($page['footer']): ?>    
+                <?php print render($page['footer']); ?>
+            <?php endif; ?>
+        </div>
+    </div>
+</footer>
